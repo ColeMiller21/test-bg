ef2c88fbab9940baa7146b897cca711d7250ce03
cole miller <cole.t.miller@gmail.com>
2024-01-29 08:07:57 -0600

    updating video on safari

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,24 @@
+"use client";
+import { useEffect, useState } from "react";
+
 import Image from "next/image";
 
 export default function Home() {
+  const [isSafari, setIsSafari] = useState(false);
+
+  useEffect(() => {
+    const ua = window.navigator.userAgent;
+    const iOS = !!ua.match(/iPad/i) || !!ua.match(/iPhone/i);
+    const webkit = !!ua.match(/WebKit/i);
+    setIsSafari(iOS && webkit && !ua.match(/CriOS/i));
+  }, []);
+
+  const safariClass = isSafari ? "safari-ios-bg-fix" : "";
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24 bg-primary">
       <h1 className="font-extrabold text-4xl">BG Test</h1>
-      <div className="relative w-full m-auto">
+      <div className={`relative w-full ${safariClass}`}>
         <video
           className={"w-full lg:w-1/2 h-full object-contain"}
           preload="auto"
